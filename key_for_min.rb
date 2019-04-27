6f2294bc4d03244f2e7a61f8a64b1e6827864464
Emily R <emilyroulund@gmail.com>
2019-04-27 00:47:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_key = nil
   smallest_value = nil
   name_hash.each do |kys, vls|
-    if smallest_value == nil || vls < smallest_value
+    if name_hash == {} || vls < smallest_value
       smallest_value = vls
       smallest_key = kys
     end
